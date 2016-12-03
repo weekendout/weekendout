f4afcd52083f06faffb646dc7bcb10b3fa18f859
Francisco Lourenco <aristidesfl@gmail.com>
2016-12-03 18:02:57 +0100

    Initial Commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <hello></hello>
+  </div>
+</template>
+
+<script lang="coffee">
+Hello = require('./components/Hello')
+
+module.exports =
+  name: 'app'
+  components: {Hello}
+
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
