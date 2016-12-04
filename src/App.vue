0f07ac5f0628bbc713015a29270e504b9cb2ab0c
Francisco Lourenco <aristidesfl@gmail.com>
2016-12-04 01:21:50 +0100

    Fist iteration of landing page

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@
 
         <!-- Header -->
           <header id="header" class="alt">
-            <h1><a href="index.html">Solid State</a></h1>
+            <h1><a href="index.html">Escape this Weekend</a></h1>
             <nav>
               <a href="#menu">Menu</a>
             </nav>
@@ -29,9 +29,9 @@
         <!-- Banner -->
           <section id="banner">
             <div class="inner">
-              <div class="logo"><span class="icon fa-diamond"></span></div>
-              <h2>This is Solid State</h2>
-              <p>Another free + fully responsive site template by <a href="http://html5up.net">HTML5 UP</a></p>
+              <h2>Take off this Weekend </h2>
+              <p>10.000+ destinations. chosen for you. ready to go.</p>
+              <div class="logo"><span class="icon fa-plane"></span> <span class="icon fa-bus"></span> <span class="icon fa-bed"></span> <span class="icon fa-suitcase"></span> <span class="icon fa-tree"></span></div>
             </div>
           </section>
 
@@ -43,9 +43,9 @@
                 <div class="inner">
                   <a href="#" class="image"><img src="~images/pic01.jpg" alt="" /></a>
                   <div class="content">
-                    <h2 class="major">Magna arcu feugiat</h2>
-                    <p>Lorem ipsum dolor sit amet, etiam lorem adipiscing elit. Cras turpis ante, nullam sit amet turpis non, sollicitudin posuere urna. Mauris id tellus arcu. Nunc vehicula id nulla dignissim dapibus. Nullam ultrices, neque et faucibus viverra, ex nulla cursus.</p>
-                    <a href="#" class="special">Learn more</a>
+                    <h2 class="major">The best deals</h2>
+                    <p>No time to waste? We find out the best deals out there so you don't have to. We apply smart search algorithms to the best partners on transport, accomodation and services, and find weekend packages which fit your taste and budget, providing the best value in the industry.</p>
+                    <a href="#footer" class="special">Try Now</a>
                   </div>
                 </div>
               </section>
@@ -55,9 +55,9 @@
                 <div class="inner">
                   <a href="#" class="image"><img src="~images/pic02.jpg" alt="" /></a>
                   <div class="content">
-                    <h2 class="major">Tempus adipiscing</h2>
-                    <p>Lorem ipsum dolor sit amet, etiam lorem adipiscing elit. Cras turpis ante, nullam sit amet turpis non, sollicitudin posuere urna. Mauris id tellus arcu. Nunc vehicula id nulla dignissim dapibus. Nullam ultrices, neque et faucibus viverra, ex nulla cursus.</p>
-                    <a href="#" class="special">Learn more</a>
+                    <h2 class="major">Always Shining</h2>
+                    <p>Our smart engine automatically filters out destinations where the weather is going to be bad</p>
+                    <a href="#footer" class="special">Try Now</a>
                   </div>
                 </div>
               </section>
@@ -67,9 +67,10 @@
                 <div class="inner">
                   <a href="#" class="image"><img src="~images/pic03.jpg" alt="" /></a>
                   <div class="content">
-                    <h2 class="major">Nullam dignissim</h2>
-                    <p>Lorem ipsum dolor sit amet, etiam lorem adipiscing elit. Cras turpis ante, nullam sit amet turpis non, sollicitudin posuere urna. Mauris id tellus arcu. Nunc vehicula id nulla dignissim dapibus. Nullam ultrices, neque et faucibus viverra, ex nulla cursus.</p>
-                    <a href="#" class="special">Learn more</a>
+                    <h2 class="major">Full Package</h2>
+                    <p>We organize everything, and our concierge will always be available to help over the phone. We provide full package* to you including transportation from your door, to the destination, and back.</p>
+                    <p>*Including services like Uber, Taxi, Bus, Flights and Accomodation.</p>
+                    <a href="#footer" class="special">Try Now</a>
                   </div>
                 </div>
               </section>
@@ -77,32 +78,32 @@
             <!-- Four -->
               <section id="four" class="wrapper alt style1">
                 <div class="inner">
-                  <h2 class="major">Vitae phasellus</h2>
+                  <h2 class="major">Testimonials</h2>
                   <p>Cras mattis ante fermentum, malesuada neque vitae, eleifend erat. Phasellus non pulvinar erat. Fusce tincidunt, nisl eget mattis egestas, purus ipsum consequat orci, sit amet lobortis lorem lacus in tellus. Sed ac elementum arcu. Quisque placerat auctor laoreet.</p>
                   <section class="features">
                     <article>
                       <a href="#" class="image"><img src="~images/pic04.jpg" alt="" /></a>
                       <h3 class="major">Sed feugiat lorem</h3>
                       <p>Lorem ipsum dolor sit amet, consectetur adipiscing vehicula id nulla dignissim dapibus ultrices.</p>
-                      <a href="#" class="special">Learn more</a>
+                      <a href="#footer" class="special">Try Now</a>
                     </article>
                     <article>
                       <a href="#" class="image"><img src="~images/pic05.jpg" alt="" /></a>
                       <h3 class="major">Nisl placerat</h3>
                       <p>Lorem ipsum dolor sit amet, consectetur adipiscing vehicula id nulla dignissim dapibus ultrices.</p>
-                      <a href="#" class="special">Learn more</a>
+                      <a href="#footer" class="special">Try Now</a>
                     </article>
                     <article>
                       <a href="#" class="image"><img src="~images/pic06.jpg" alt="" /></a>
                       <h3 class="major">Ante fermentum</h3>
                       <p>Lorem ipsum dolor sit amet, consectetur adipiscing vehicula id nulla dignissim dapibus ultrices.</p>
-                      <a href="#" class="special">Learn more</a>
+                      <a href="#footer" class="special">Try Now</a>
                     </article>
                     <article>
                       <a href="#" class="image"><img src="~images/pic07.jpg" alt="" /></a>
                       <h3 class="major">Fusce consequat</h3>
                       <p>Lorem ipsum dolor sit amet, consectetur adipiscing vehicula id nulla dignissim dapibus ultrices.</p>
-                      <a href="#" class="special">Learn more</a>
+                      <a href="#footer" class="special">Try Now</a>
                     </article>
                   </section>
                   <ul class="actions">
@@ -158,21 +159,11 @@
 </template>
 
 <script lang="coffee">
-Hello = require('./components/Hello')
-
 module.exports =
   name: 'app'
-  components: {Hello}
 
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
